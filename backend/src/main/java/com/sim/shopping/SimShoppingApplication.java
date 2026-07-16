7a1438a9dffb174f26007c920067c7c2bd7a7340
Developer <dev@sim-shopping.com>
2026-07-16 21:31:33 +0800

    build(backend): 支持打包为war包并更新配置
    - 修改.env及application-local.yml中的数据库密码配置为shop@2026
    - 更新文件上传路径为 /usr/bear/uploads
    - 在pom.xml中添加war包打包配置和finalName设置
    - SimShoppingApplication类继承SpringBootServletInitializer以支持war部署
    - 覆写configure方法，配置SpringApplicationBuilder实例化应用程序类

--- backend/src/main/java/com/sim/shopping/SimShoppingApplication.java
+++ backend/src/main/java/com/sim/shopping/SimShoppingApplication.java
@@ -3,14 +3,21 @@ package com.sim.shopping;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.sim.shopping.infrastructure.persistence.mapper")
 @EnableScheduling
-public class SimShoppingApplication {
+public class SimShoppingApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SimShoppingApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SimShoppingApplication.class);
     }
 }
